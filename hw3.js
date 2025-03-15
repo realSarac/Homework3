81c19de12829579185be5ed600f1507f423c7aa1
realSarac <4452420@taibahu.edu.sa>
2025-03-15 23:57:45 +0300

    Create hw3.js

---
+++ hw3.js
@@ -0,0 +1,3 @@
+document.getElementById("signupForm").onsubmit = function(event) {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
