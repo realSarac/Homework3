74f21d2f0de5b64b698137d41d479ae0a154a52a
realSarac <4452420@taibahu.edu.sa>
2025-03-15 23:58:40 +0300

    Create hw3.php

---
+++ hw3.php
@@ -0,0 +1,5 @@
+<?php
+$servername = "localhost";
+$username = "root";  // اسم المستخدم لقاعدة البيانات
+$password = "";  // كلمة مرور قاعدة البيانات (اتركه فارغًا في XAMPP)
+$dbname = "Homework";  // اسم قاعدة البيانات
