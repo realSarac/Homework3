e2d33265fdf724ef542b0afe0dab94d283a5fac1
ax7i8 <4450743@taibahu.edu.sa>
2025-03-19 06:24:08 +0300

    Update hw3.php

--- hw3.php
+++ hw3.php
@@ -1,5 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";  // اسم المستخدم لقاعدة البيانات
-$password = "";  // كلمة مرور قاعدة البيانات (اتركه فارغًا في XAMPP)
+$password = "";  // كلمة مرور قاعدة البيانات 
 $dbname = "Homework";  // اسم قاعدة البيانات
+$conn = mysqli_connect ($servername , $username,$password ,$dbname) ;
+if (!$conn){
+die ("not connected-غير متصل" .mysqli_error());
+}
+?>
